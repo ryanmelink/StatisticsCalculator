602d899f0ce905709773ef41181cc089d319d781
kb8njit <kb8@njit.edu>
2020-07-03 15:00:10 -0400

    Fix:  Fixed code in CSVReader.py

--- CSVReader/CSVReader.py
+++ CSVReader/CSVReader.py
@@ -1,27 +1,26 @@
 import csv
 from FileUtilities.Absolute_Path import absolute_path
-
-
-
-import csv
 from pprint import pprint
 
-    def class_factory(class_name, dictionary):
-        return type(class_name, (object,), dictionary)
 
-    class CSVReader:
-        data = []
+def class_factory(class_name, dictionary):
+    return type(class_name, (object,), dictionary)
 
-        def __init__(self, filepath):
-            self.data[]
-            with open(absolute_path(filepath)) as test_data:
-                csv_reader = csv.DictReader(test_data, delimiter=',')
-                for row in csv_reader:
-                    self.data.append(row)
-            pass
 
-        def return_data_as_object(self, class_name):
-            objects = []
-            for row in self.data:
-                objects.append(class_factory(class_name, row))
-            return objects
+class CsvReader:
+    data = []
+
+    def __init__(self, filepath):
+        self.data = []
+        with open(absolute_path(filepath)) as text_data:
+            csv_data = csv.DictReader(text_data, delimiter=',')
+            for row in csv_data:
+                self.data.append(row)
+                pprint(self.data)
+        pass
+
+    def return_data_as_objects(self, class_name):
+        objects = []
+        for row in self.data:
+            objects.append(class_factory(class_name, row))
+        return objects
