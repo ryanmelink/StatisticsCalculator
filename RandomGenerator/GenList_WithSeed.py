ad294be564ea8498ae3f86bd1acb6f7dcb6d5e0d
kb8njit <kb8@njit.edu>
2020-07-05 15:01:05 -0400

    Task: Added Random generator files

---
+++ RandomGenerator/GenList_WithSeed.py
@@ -0,0 +1,1 @@
+#Generate a list of N random numbers with a seed and between a range of numbers - Both Integer and Decimal
---
+++ RandomGenerator/GenNum_WithSeed.py
@@ -0,0 +1,1 @@
+#Generate a random number with a seed between a range of two numbers - Both Integer and Decimal
