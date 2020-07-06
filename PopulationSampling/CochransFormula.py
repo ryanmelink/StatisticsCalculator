532ed793a349cb64472c78f200a0b3b158ebc1ff
kb8njit <kb8@njit.edu>
2020-07-06 16:07:00 -0400

    Feature: Added code to CochransFormula.py

--- PopulationSampling/CochransFormula.py
+++ PopulationSampling/CochransFormula.py
@@ -1,5 +1,20 @@
 #Cochran’s Sample Size Formula
-# from Calculator.Multiplication import multiplication
-# from Calculator.Division import division
+from Calculator.Subtraction import subtraction
+from Calculator.Multiplication import multiplication
+from Calculator.Division import division
 
 #= ((Z-Value)^2 * (p) * (q)) / (e)^2
+
+
+def cochran(p, q, z, e):
+    try:
+        n1 = multiplication(p, q)
+        n2 = z * z
+        n3 = multiplication(n1, n2)
+        n4 = e * e
+        rCochran = division(n4, n3)
+        return float(rCochran)
+    except ZeroDivisionError:
+        print("Error! Cannot Divide by 0")
+    except ValueError:
+        print("Error! Check data inputs")
