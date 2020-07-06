469c54e73e29b09bd54b1ec4b7c2a930cdcd55f0
Monica <mjn28@njit.edu>
2020-07-06 16:56:35 -0400

    Feature: Created ListItems_NoSeed static methods

--- RandomGenerator/ListItems_NoSeed.py
+++ RandomGenerator/ListItems_NoSeed.py
@@ -1,1 +1,10 @@
 #Select N number of items from a list without a seed
+
+from random import randint
+
+class ListItemNoSeed:
+    @staticmethod
+    def list(series):
+        length = len(series)
+        position = randint(0, length-1)
+        return series[position]
