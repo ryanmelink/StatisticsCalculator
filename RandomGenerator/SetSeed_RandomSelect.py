8b4efe4f50244510cba56dfebf822967fec6a63e
Monica <mjn28@njit.edu>
2020-07-06 18:14:03 -0400

    Feature: Created SetSeed_RandomSelect static method

--- RandomGenerator/SetSeed_RandomSelect.py
+++ RandomGenerator/SetSeed_RandomSelect.py
@@ -1,1 +1,12 @@
 #Set a seed and randomly.select the same value from a list
+
+from random import seed
+from RandomGenerator.Select_RandomItem import SelectRandomItem
+
+class SetSeedRandomSelect:
+
+    @staticmethod
+    def set_random_seed_select(thelist, ranges, nut):
+        seed(nut)
+        thelist2 = SelectRandomItem.select_random_item(thelist, ranges)
+        return thelist2
