69121f7c270d6e49d1658cce75e42cb8680ad9f3
kb8njit <kb8@njit.edu>
2020-07-06 17:39:42 -0400

    Feature: Added Random Sampling code and test items

--- Tests/test_PopSampling.py
+++ Tests/test_PopSampling.py
@@ -8,7 +8,7 @@ class MyTestCase(unittest.TestCase):
     test_FindSampleSize = CSVReader('')
     test_ConfidenceInterval = CSVReader('')
     test_MarginOfError = CSVReader('')
-    test_RandomSampling = CSVReader('')
+    test_RandomSampling = CSVReader('Tests/Data_PopSample/RandomSampling_Test.csv').data
 
     def setUp(self):
         self.PopSampling = PopSampling()
@@ -22,18 +22,19 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.PopSampling.result, float(row['a']))
 
     #def test_confidence_interval(self):
-        for row in self.test_ConfidenceInterval:
+        #for row in self.test_ConfidenceInterval:
 
     #def test_find_sample_size(self):
-        for row in self.test_FindSampleSize:
+        #for row in self.test_FindSampleSize:
 
     #def test_margin_of_error(self):
-        for row in self.test_MarginOfError:
+        #for row in self.test_MarginOfError:
 
-    #def test_random_sampling(self):
+    def test_random_sampling(self):
         for row in self.test_RandomSampling:
             series = [row['Value1'], row['Value2'], row['Value3'], row['Value4'], row['Value5']]
-
+            value = self.PopSampling.random_sampling(series, int(row['Choose']))
+            self.assertEqual(len(value), int(row['Choose']))
 
 
 
