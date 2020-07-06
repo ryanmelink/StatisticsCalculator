84fab4f73435b8864f7df8f3893b3ba31e7a56ee
Monica <mjn28@njit.edu>
2020-07-06 17:45:16 -0400

    Feature: Created Select_RandomItem static method

--- RandomGenerator/Select_RandomItem.py
+++ RandomGenerator/Select_RandomItem.py
@@ -1,1 +1,14 @@
 #Select a random item from a list
+
+from random import randint
+
+class SelectRandomItem:
+
+    @staticmethod
+    def select_random_item(thelist, ranges):
+        list2 = []
+        length=len(thelist)
+        for each in range(ranges):
+            position = randint(0, length-1)
+            thelist.append(thelist[position])
+        return list2
