6ce626ac75ee174d43d8ec481b4bdf018770887f
kb8njit <kb8@njit.edu>
2020-07-07 19:42:57 -0400

    Task: Creating FindSampleSize code and test

--- PopulationSampling/FindSampleSize.py
+++ PopulationSampling/FindSampleSize.py
@@ -1,4 +1,27 @@
 #How to Find a Sample Size Given a Confidence Interval and Width (unknown population standard deviation)
 
-#from Calculator.Square_rooted import square_rooted
-#from StatCalculator.Mean import mean
+from Calculator.Squared import squared
+from Calculator.Multiplication import multiplication
+from Calculator.Division import division
+
+
+#Sample Size = (^p * ^q) * (Za2 / E)^2
+
+
+def find_sample_size(p, q, za2, e):
+    try:
+        p = float(p)
+        q = float(q)
+        za2 = float(za2)
+        e = float(e)
+
+        num1 = multiplication(p, q)
+        num2 = division(za2, e)
+        num3 = squared(num2)
+        result = multiplication(num1, num3)
+        return result
+
+    except ZeroDivisionError:
+        print('Error! Cannot divide by 0')
+    except ValueError:
+        print('Error! Invalid data entry')
--- PopulationSampling/PopSampling.py
+++ PopulationSampling/PopSampling.py
@@ -1,5 +1,7 @@
 from Calculator.Calculator import Calculator
 from PopulationSampling.CochransFormula import cochran
+from PopulationSampling.FindSampleSize import find_sample_size
+
 
 class PopSampling(Calculator):
     data = []
@@ -9,4 +11,8 @@ class PopSampling(Calculator):
 
     def cochran(self, z, p, q, e):
         self.result = cochran(z, p, q, e)
+        return self.result
+
+    def find_sample_size(self, p, q, za2, e):
+        self.result = find_sample_size(p, q, za2, e)
         return self.result
